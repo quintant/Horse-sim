57e14edf657fbe76242a7ccb6d3e3f0df15d76ef
Sigurjon Ingi <sigurjoni52@gmail.com>
2021-12-20 01:57:39 +0000

    first commit

---
+++ app.py
@@ -0,0 +1,88 @@
+import pygame
+from colors import *
+from pygame.locals import *
+
+from horse import Horse
+
+
+class App:
+    def __init__(self):
+        self._running = True
+        self._display_surf = None
+        self.size = self.width, self.height = 1200, 900
+        self.font = None
+        self.num_horses = None
+        self.clock = pygame.time.Clock()
+        self.horses = []
+        self.div = 100
+
+    def on_init(self):
+        pygame.init()
+        self._display_surf = pygame.display.set_mode(self.size)
+        self._running = True
+        pygame.display.set_caption("Horse Betting Sim")
+        self.font = pygame.font.Font("freesansbold.ttf", 32)
+        self.get_num_horses()
+        self.horses = [Horse([0, 100 + self.div*i], self._display_surf) for i in range(self.num_horses)]
+
+    def on_event(self, event):
+        if event.type == pygame.QUIT:
+            self._running = False
+
+    def on_loop(self):
+        for horse in self.horses:
+            horse.step()
+
+    def on_render(self):
+        self._display_surf.fill(black)
+        for horse in self.horses:
+            horse.blit()
+        pygame.display.update()
+
+    def on_cleanup(self):
+        pygame.quit()
+
+    def on_execute(self):
+        if self.on_init() == False:
+            self._running = False
+
+        while self._running:
+            self.clock.tick(60)
+            for event in pygame.event.get():
+                self.on_event(event)
+            self.on_loop()
+            self.on_render()
+        self.on_cleanup()
+
+    def get_num_horses(self):
+        def disp(n):
+            self._display_surf.fill(black)
+            text = self.font.render("How many horses?", True, green, blue)
+            textRect = text.get_rect()
+            textRect.center = (self.width // 2, self.height // 2)
+            self._display_surf.blit(text, textRect)
+            # Num horses
+            text = self.font.render(n, True, green, blue)
+            textRect = text.get_rect()
+            textRect.center = (self.width // 2, self.height // 2 + 32 + 2)
+            self._display_surf.blit(text, textRect)
+            pygame.display.update()
+
+        num = ""
+        done = False
+        while not done:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self._running = False
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_RETURN:
+                        if num.isdigit():
+                            done = True
+                            self.num_horses = int(num)
+                        else:
+                            num = ""
+                    elif event.key == pygame.K_BACKSPACE:
+                        num = num[:-1]
+                    else:
+                        num += event.unicode
+            disp(num)
---
+++ horse.py
@@ -0,0 +1,35 @@
+from random import uniform, randint
+import random
+from pygame.draw import circle
+from colors import *
+
+class Horse:
+    def __init__(self, pos:list, surface) -> None:
+        """X/Y pos"""
+        self.pos = pos
+        self.init_speed = uniform(.3, .5)
+        self.stamina = randint(100, 300)
+        self.sprint_multiplier = uniform(3, 5)
+        self.threshold = 0.8
+        self.surface = surface
+        self.sprinting = False
+        self.sp_cnt = 1
+        self.stam_t_mod = uniform(0.8, 1.2)
+
+    def step(self):
+        if self.sprinting:
+            self.pos[0] += self.init_speed * self.sprint_multiplier
+            self.stamina -= 10
+        else:
+            self.pos[0] += self.init_speed
+            self.stamina += 1
+            x = random.normalvariate(0, 1)
+            if x > self.threshold and self.stamina > 500*self.stam_t_mod:
+                self.sp_cnt+=1
+                self.sprinting = True
+        if self.stamina < 0:
+            self.sprinting = False
+        
+    def blit(self):
+        circle(self.surface, blue, self.pos, 20)
+    
